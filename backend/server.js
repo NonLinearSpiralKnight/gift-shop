bc58154edc29b46187f04d1e0700085fcb7cd9b1
Dmitriy Zaitsev <deadsockunicorn@gmail.com>
2026-06-23 21:10:06 +0400

    ensure API routes connected

--- backend/server.js
+++ backend/server.js
@@ -4,20 +4,13 @@ const cors = require('cors');
 const productsRoutes = require('./routes/products.routes');
 
 const app = express();
-const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/health', (req, res) => {
-    res.json({
-        status: 'ok',
-        message: 'GiftShop API is running',
-    });
-});
-
 app.use('/api/products', productsRoutes);
 
-app.listen(PORT, () => {
-    console.log(`Server started on http://localhost:${PORT}`);
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
 });
+
