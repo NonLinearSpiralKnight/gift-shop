0ce1086231e0b245d96b4630cb0a27eb1133f286
Danila <elvinuborunduk@gmail.com>
2026-06-23 21:03:24 +0400

    expand database schema with categories

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,15 +1,17 @@
 DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS categories;
+
+CREATE TABLE categories (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+);
 
 CREATE TABLE products (
-
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-
     name TEXT NOT NULL,
-
     description TEXT NOT NULL,
-
     price REAL NOT NULL,
-
-    image TEXT NOT NULL
-
+    image TEXT NOT NULL,
+    category_id INTEGER,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
 );
