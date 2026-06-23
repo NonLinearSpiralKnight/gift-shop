b87e13317400e75fbab5038112bfc76ad599a63a
Dmitriy Zaitsev <deadsockunicorn@gmail.com>
2026-06-23 21:54:17 +0400

    connect API to SQLite database

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 
-const db = new sqlite3.Database('./backend/db/giftshop.sqlite', (err) => {
+const db = new sqlite3.Database('./db/giftshop.sqlite', (err) => {
     if (err) {
         console.error('Database error:', err.message);
     } else {
